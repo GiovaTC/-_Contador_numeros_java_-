c71b686d40b6ae3e547c078c31d6e002bf0e275b
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-10 11:12:06 -0500

    commit - Conexion .

--- src/main/java/com/ejemplo/contador/Conexion.java
+++ src/main/java/com/ejemplo/contador/Conexion.java
@@ -1,4 +1,19 @@
 package com.ejemplo.contador;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexion {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/contador_numeros";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "Tapiero123";
+
+    public static Connection conectar() throws Exception {
+
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }       
 }
